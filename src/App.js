a6fc3a51a0d9357bc7208635eed0dacb04c13fb8
Anoof Shaikh <anoofs@gmail.com>
2021-11-14 20:21:56 +0530

    fixed palette route in react router v6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import React, { Component } from "react";
+import { useParams } from "react-router";
 import { Route, Routes } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
-function App() {
-	return (
-		<Routes>
-			<Route exact path="/" element={<h1>PALETTE LIST GOES HERE</h1>} />
-			<Route
-				exact
-				path="/palette/:id"
-				element={<h1>INDIVIDUAL PALETTE</h1>}
-			/>
-		</Routes>
-		// <div>
-		//   <Palette palette={generatePalette(seedColors[4])} />
-		// </div>
-	);
+function findPalette(id) {
+    return seedColors.find(function(palette) {
+        return palette.id === id;
+    });
+}
+function PaletteWrapper() {
+    const { id } = useParams();
+    return (
+        <Palette
+            palette={generatePalette(
+                findPalette(id)
+            )}
+        />
+    )
+}
+class App extends Component {
+	render() {
+		return (
+			<Routes>
+				<Route exact path="/" element={<h1>PALETTE LIST GOES HERE</h1>} />
+				<Route
+					exact
+					path="/palette/:id"
+					element={<PaletteWrapper />}
+				/>
+			</Routes>
+		)
+	}
 }
 
 export default App;
