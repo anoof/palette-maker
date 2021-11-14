57716898ac783bfb1bbf0700bb41e5ab72984d82
Anoof Shaikh <anoofs@gmail.com>
2021-11-14 20:47:47 +0530

    added palettelist and links

--- src/App.js
+++ src/App.js
@@ -4,34 +4,25 @@ import { Route, Routes } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
+import PaletteList from "./PaletteList";
 
 function findPalette(id) {
-    return seedColors.find(function(palette) {
-        return palette.id === id;
-    });
+	return seedColors.find(function (palette) {
+		return palette.id === id;
+	});
 }
 function PaletteWrapper() {
-    const { id } = useParams();
-    return (
-        <Palette
-            palette={generatePalette(
-                findPalette(id)
-            )}
-        />
-    )
+	const { id } = useParams();
+	return <Palette palette={generatePalette(findPalette(id))} />;
 }
 class App extends Component {
 	render() {
 		return (
 			<Routes>
-				<Route exact path="/" element={<h1>PALETTE LIST GOES HERE</h1>} />
-				<Route
-					exact
-					path="/palette/:id"
-					element={<PaletteWrapper />}
-				/>
+				<Route exact path="/" element={<PaletteList palettes={seedColors} />} />
+				<Route exact path="/palette/:id" element={<PaletteWrapper />} />
 			</Routes>
-		)
+		);
 	}
 }
 
--- src/NavBar.js
+++ src/NavBar.js
@@ -7,6 +7,7 @@ import MenuItem from "@material-ui/core/MenuItem";
 import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   constructor(props) {
@@ -28,7 +29,7 @@ class Navbar extends Component {
     return (
       <header className='Navbar'>
         <div className='logo'>
-          <a href='#'>palettepicker</a>
+          <Link to="/">Palette Maker</Link>
         </div>
         <div className='slider-container'>
           <span>Level: {level}</span>
